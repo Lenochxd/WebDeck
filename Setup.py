ea440d1291c909b5fc746febc5076fe39cd8e65b
LeLenoch <81lennoch@gmail.com>
2023-08-12 12:42:22 +0200

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,23 @@
+import sys, os, json
+from cx_Freeze import setup, Executable
+
+with open('config.json', 'r', encoding="utf-8") as f:
+    json_file = json.load(f)
+    version = json['version']
+    
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {
+    # "excludes": ["tkinter", "unittest"],
+    # "zip_include_packages": ["encodings", "PySide6"],
+}
+
+# base="Win32GUI" should be used only for Windows GUI app
+base = "Win32GUI" if sys.platform == "win32" else None
+
+setup(
+    name="WebDeck",
+    version=f"{version}",
+    description="its free!",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("main.py", base=base)],
+)
