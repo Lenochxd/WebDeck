a397669be39cd2821c9b338e42040ef566235d99
LeLenoch <81lennoch@gmail.com>
2023-08-13 01:31:53 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -6,15 +6,15 @@ D:\Users\81len\AppData\Roaming\Python\Python311\Scripts\pyinstaller --noconfirm 
 D:\Users\81len\AppData\Roaming\Python\Python311\Scripts\pyinstaller --noconfirm --onedir --console --name "WD_main" "%current_dir%/main_server.py"
 
 REM déplacer le dossier /dist/WD_main dans /dist/WebDeck
-move "%current_dir%\dist\WD_main\*" "%current_dir%\dist\WebDeck\"
+move /y "%current_dir%\dist\WD_main\*" "%current_dir%\dist\WebDeck\"
 
 D:\Users\81len\AppData\Roaming\Python\Python311\Scripts\pyinstaller --noconfirm --onedir --windowed --name "WD_start" "%current_dir%/start_server.py"
 
 REM déplacer le dossier /dist/WD_start dans /dist/WebDeck
-move "%current_dir%\dist\WD_start\*" "%current_dir%\dist\WebDeck\"
+move /y "%current_dir%\dist\WD_start\*" "%current_dir%\dist\WebDeck\"
 
 REM copier racine dans /dist/WebDeck
-move "%current_dir%\dist\WebDeck\Webdeck\*" "%current_dir%\dist\Webdeck\"
+move /y "%current_dir%\dist\WebDeck\Webdeck\*" "%current_dir%\dist\Webdeck\"
 
 
 echo Files moved successfully.
