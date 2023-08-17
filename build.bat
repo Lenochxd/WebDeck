ae70c09cb0cbf92e2de0696fdb081adc230c520a
LeLenoch <81lennoch@gmail.com>
2023-08-17 17:11:23 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,19 +2,22 @@
 
 for %%I in ("%~dp0") do set "current_dir=%%~fI"
 
-D:\Users\81len\AppData\Roaming\Python\Python311\Scripts\pyinstaller --noconfirm --onedir --windowed --name "WebDeck" --add-data "%current_dir%;WebDeck/" --icon "%current_dir%/static/files/icon.ico" "%current_dir%/main.py"
-D:\Users\81len\AppData\Roaming\Python\Python311\Scripts\pyinstaller --noconfirm --onedir --console --name "WD_main" "%current_dir%/main_server.py"
-
-REM déplacer le dossier /dist/WD_main dans /dist/WebDeck
-move /y "%current_dir%\dist\WD_main\*" "%current_dir%\dist\WebDeck\"
-
-D:\Users\81len\AppData\Roaming\Python\Python311\Scripts\pyinstaller --noconfirm --onedir --windowed --name "WD_start" "%current_dir%/start_server.py"
-
-REM déplacer le dossier /dist/WD_start dans /dist/WebDeck
-move /y "%current_dir%\dist\WD_start\*" "%current_dir%\dist\WebDeck\"
+D:\Users\81len\AppData\Roaming\Python\Python311\Scripts\pyinstaller --noconfirm --onefile --windowed --name "WebDeck" --icon "%current_dir%/static/files/icon.ico" "%current_dir%/main.py"
 
 REM copier racine dans /dist/WebDeck
-move /y "%current_dir%\dist\WebDeck\Webdeck\*" "%current_dir%\dist\Webdeck\"
+xcopy "%current_dir%\*" "%current_dir%\dist\" /s /y
+
+D:\Users\81len\AppData\Roaming\Python\Python311\Scripts\pyinstaller --noconfirm --onefile --console --name "WD_main" "%current_dir%/main_server.py"
+
+REM déplacer le dossier /dist/WD_main dans /dist/WebDeck
+xcopy "%current_dir%\dist\WD_main\*" "%current_dir%\dist\WebDeck\" /s /y
+@RD /S /Q "%current_dir%\dist\WD_main"
+
+D:\Users\81len\AppData\Roaming\Python\Python311\Scripts\pyinstaller --noconfirm --onefile --windowed --name "WD_start" "%current_dir%/start_server.py"
+
+REM déplacer le dossier /dist/WD_start dans /dist/WebDeck
+xcopy "%current_dir%\dist\WD_start\*" "%current_dir%\dist\WebDeck\" /s /y
+@RD /S /Q "%current_dir%\dist\WD_start"
 
 
 echo Files moved successfully.
