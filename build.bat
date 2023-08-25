4994637ff115ca253523de6216c8911fd284eb66
LeLenoch <81lennoch@gmail.com>
2023-08-25 18:48:59 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,9 @@
+python -m venv webdeck
+webdeck\Scripts\activate.bat
+pip install -r requirements.txt
 python setup.py build
+
+cd build
+cd exe.win-amd64-3.11
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool" sign /a /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 WebDeck.exe
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool" sign /a /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 WD_main.exe
