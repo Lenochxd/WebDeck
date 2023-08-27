31e24de42c6064fb7cc950691e96c9916156bc71
LeLenoch <81lennoch@gmail.com>
2023-08-27 20:09:18 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,28 +1,29 @@
 @echo off
 echo Starting...
 
-rem Créer un environnement virtuel
+rem Create a virtual environment
 python -m venv webdeck
 
-rem Activer l'environnement virtuel
+rem Activate the virtual environment
 call webdeck\Scripts\activate.bat
 
-rem Installer les dépendances
+rem Install dependencies
 pip install -r requirements.txt
 
-rem Compiler le projet
+rem Compile the project
 python setup.py build
 
-rem Naviguer vers le répertoire de build
-cd build\exe.win-amd64-3.11
+rem Navigate to the build directory
+cd build
+cd exe.win-amd64-3.11
 
-rem Signer le premier exécutable
+rem Sign the first executable
 "C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool" sign /a /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 WebDeck.exe
 
-rem Signer le deuxième exécutable
+rem Sign the second executable
 "C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool" sign /a /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 WD_main.exe
 
-rem Signer le troisième exécutable
+rem Sign the third executable
 "C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool" sign /a /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 WD_updater.exe
 
-echo Toutes les commandes ont été exécutées avec succès.
+echo Build done!
