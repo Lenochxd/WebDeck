aeba4c91f944ced862fe65ed1cf91ee25272aba9
Lenoch <81lennoch@gmail.com>
2023-08-31 18:33:34 +0200

    Delete testmic directory

--- testmic/playsound.py
+++
@@ -1,9 +0,0 @@
-import sys
-import win10toast
-
-# Lire l'information envoyée via stdin
-information = sys.stdin.read()
-
-print(information)
-toaster = win10toast.ToastNotifier()
-toaster.show_toast(information, information, duration=10)
