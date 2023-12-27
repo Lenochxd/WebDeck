f51d7df60bba004688674029fb1e66870d2dd43f
LeLenoch <81lennoch@gmail.com>
2023-12-27 22:11:18 +0100

    Create upload_file.js

---
+++ static/files/upload_file.js
@@ -0,0 +1,26 @@
+function upload_file(element) {
+    var file = element.files[0];
+    var formData = new FormData();
+    formData.append('file', file);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/upload_file', true);
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            console.log('Fichier téléchargé avec succès!');
+        } else {
+            console.error('Échec du téléchargement du fichier.');
+        }
+    };
+
+    xhr.send(formData);
+}
+
+// new audio file -> **uploaded
+var elements = document.querySelectorAll('.audio-input');
+elements.forEach(function(element) {
+    element.addEventListener('change', function() {
+        upload_file(element);
+    });
+});
