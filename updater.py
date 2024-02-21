e0bef36b8d57554250950890ae0efaff7312652d
LeLenoch <81lennoch@gmail.com>
2024-02-21 01:17:34 +0100

    Update updater.py
    Fixed: not working when needed to update the updater itself or the lib directory
    Fixed: downloads the latest version even if it is already installed

--- updater.py
+++ updater.py
@@ -3,7 +3,6 @@ import ctypes, sys
 if not ctypes.windll.shell32.IsUserAnAdmin():
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
     sys.exit()
-    exit()
 
 import os
 import shutil
@@ -72,7 +71,7 @@ def compare_versions(version1, version2):
 
 
 def check_updates(current_version):
-    url = "https://api.github.com/repos/LeLenoch/WebDeck/releases/latest"
+    url = "https://api.github.com/repos/Lenochxd/WebDeck/releases/latest"
     response = requests.get(url)
     data = response.json()
 
@@ -80,41 +79,40 @@ def check_updates(current_version):
     if compare_versions(latest_version, current_version) > 0:
         print(f"New version available: {latest_version}")
 
-    if os.path.exists("WebDeck-update"):
-        open_process("WebDeck.exe")
-        shutil.rmtree("WebDeck-update")
-        if os.path.exists("WebDeck-update.zip"):
-            os.remove("WebDeck-update.zip")
-    else:
+        if os.path.exists("WebDeck-update"):
+            open_process("WebDeck.exe")
+            shutil.rmtree("WebDeck-update")
+            if os.path.exists("WebDeck-update.zip"):
+                os.remove("WebDeck-update.zip")
+        else:
 
-        process_names = ["WebDeck.exe", "WD_main.exe"]
-        close_processes(process_names)
-        for file_url in data["assets"]:
-            if file_url["browser_download_url"].endswith('.zip'):
-                download_and_extract(file_url["browser_download_url"])
-                break
-                
-        delete_files = []
-        try:
-            version_json_url = "https://raw.githubusercontent.com/LeLenoch/WebDeck/master/static/files/version.json"
-            response_json = requests.get(version_json_url)
-            for version in version_json_url.json():
-                if version['version'] == latest_version and "deleted_files" in version:
-                    delete_files = version["deleted_files"]
+            process_names = ["WebDeck.exe", "WD_main.exe"]
+            close_processes(process_names)
+            for file_url in data["assets"]:
+                if file_url["browser_download_url"].endswith('.zip'):
+                    download_and_extract(file_url["browser_download_url"])
                     break
-        except Exception:
-            pass
-                
-        for filename in delete_files:
-            if os.path.exists(filename):
-                if os.path.isfile(filename):
-                    os.remove(filename)
-                elif os.path.isdir(filename):
-                    os.rmdir(filename)
+                    
+            delete_files = []
+            try:
+                version_json_url = "https://raw.githubusercontent.com/Lenochxd/WebDeck/master/static/files/version.json"
+                for version in version_json_url.json():
+                    if version['version'] == latest_version and "deleted_files" in version:
+                        delete_files = version["deleted_files"]
+                        break
+            except Exception:
+                pass
+                    
+            for filename in delete_files:
+                if os.path.exists(filename):
+                    if os.path.isfile(filename):
+                        os.remove(filename)
+                    elif os.path.isdir(filename):
+                        os.rmdir(filename)
 
-        replace_files()
+            replace_files()
 
-        open_process("WebDeck.exe")
+            open_process("WebDeck.exe")
 
 def download_and_extract(download_url):
     response = requests.get(download_url, stream=True)
@@ -129,16 +127,22 @@ def download_and_extract(download_url):
     else:
         error("Failed to download update ZIP file.")
 
-def replace_files():
-    current_dir = os.path.abspath(os.path.dirname(__file__))
+#TESTING
+# def download_and_extract(download_url):
+#     shutil.copyfile("E:/Users/81len/Downloads/WebDeck-fake-update.zip", "WebDeck-update.zip")
+#     with zipfile.ZipFile('WebDeck-update.zip', 'r') as zip_ref:
+#         zip_ref.extractall('WebDeck-update')
+#     return
 
-    # Move files and folders from WebDeck-update/WebDeck to the current directory
+def replace_files():
+    # Move files and folders from WebDeck-update/WebDeck to the wd_dir directory
+    print('Moving static/updates/WebDeck-update/WebDeck to root')
     update_dir = os.path.join(current_dir, "WebDeck-update", "WebDeck")
     for root, _, files in os.walk(update_dir):
         for item in files:
             source = os.path.join(root, item)
             relative_path = os.path.relpath(source, update_dir)
-            destination = os.path.join(current_dir, relative_path)
+            destination = os.path.join(wd_dir, relative_path)
             try:
                 os.makedirs(os.path.dirname(destination), exist_ok=True)
                 shutil.move(source, destination)
@@ -146,19 +150,30 @@ def replace_files():
                 print(e)
 
     # Remove the WebDeck-update directory
+    print('Removing WebDeck-update directory')
     update_dir_path = os.path.join(current_dir, "WebDeck-update")
     shutil.rmtree(update_dir_path, ignore_errors=True)
 
     # Delete the WebDeck-update.zip file
     zip_file_path = os.path.join(current_dir, "WebDeck-update.zip")
     os.remove(zip_file_path)
+    print('WebDeck-update.zip deleted')
 
-    # Launch WebDeck.exe
-    exe_path = os.path.join(current_dir, "WebDeck.exe")
+    # Launch WebDeck.exe from the wd_dir (root) directory
+    print('Restarting WebDeck.exe')
+    exe_path = os.path.join(wd_dir, "WebDeck.exe")
     os.system(exe_path)
 
 
-
-with open('static/files/version.json', encoding="utf-8") as f:
-    current_version = json.load(f)['versions'][0]['version']
-check_updates(current_version)
+# if __name__ != "__main__":
+if __name__ == "__main__":
+    print('Starting updater...')
+    
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    wd_dir = os.path.abspath(os.path.join(current_dir, *([".."] * 2)))
+    version_path = os.path.join(wd_dir, 'static/files/version.json')
+    
+    with open(version_path, encoding="utf-8") as f:
+        current_version = json.load(f)['versions'][0]['version']
+        
+    check_updates(current_version)
