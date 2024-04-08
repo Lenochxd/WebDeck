855b4a949ccbddc91f53a3cc381840e0c0d2af10
Lenochxd <81lennoch@gmail.com>
2024-04-08 23:10:52 +0200

    Fixed updater error
    Fixed updater not being able to restart webdeck after update

--- updater.py
+++ updater.py
@@ -173,7 +173,7 @@ def check_updates(current_version):
         # Launch WebDeck.exe from the wd_dir (root) directory
         print("Restarting WebDeck.exe")
         exe_path = os.path.join(wd_dir, "WebDeck.exe")
-        os.system(f"start {exe_path}")
+        os.system(f'start "{exe_path}"')
 
 
 def download_and_extract(download_url):
