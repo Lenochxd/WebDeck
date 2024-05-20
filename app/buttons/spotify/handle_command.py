8a490e614b38eece62b55ab5a168b3efd3833025
Lenochxd <81lennoch@gmail.com>
2024-05-20 17:15:21 +0200

    Tidy up: spotify init file

--- app/buttons/spotify/handle_command.py
+++ app/buttons/spotify/handle_command.py
@@ -1,5 +1,4 @@
 import spotipy
-import spotipy.util as util
 import json
 
 import app.buttons.spotify.albums as album
@@ -8,25 +7,7 @@ import app.buttons.spotify.playlists as playlist
 import app.buttons.spotify.artists as artist
 import app.buttons.spotify.volume as volume
 
-
-with open('config.json', encoding= "utf-8") as f:
-    config = json.load(f)
-    
-def initialize():
-    # Set up the Spotify API client
-    try:
-        spotify_redirect_uri = "http://localhost:8888/callback"
-        spotify_scope = "user-library-modify user-library-read user-read-currently-playing user-read-playback-state user-modify-playback-state playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read"
-        return util.prompt_for_user_token(
-            config["settings"]["spotify-api"]["USERNAME"],
-            spotify_scope,
-            config["settings"]["spotify-api"]["CLIENT_ID"],
-            config["settings"]["spotify-api"]["CLIENT_SECRET"],
-            spotify_redirect_uri,
-        )
-    except Exception as e:
-        print(f'ERROR: Failed to start spotipy, {e}')
-
+from .utils import initialize
 spotify_token = initialize()
 
 
---
+++ app/buttons/spotify/utils.py
@@ -0,0 +1,22 @@
+import spotipy.util as util
+import json
+
+
+with open('config.json', encoding= "utf-8") as f:
+    config = json.load(f)
+
+
+def initialize():
+    # Set up the Spotify API client
+    try:
+        spotify_redirect_uri = "http://localhost:8888/callback"
+        spotify_scope = "user-library-modify user-library-read user-read-currently-playing user-read-playback-state user-modify-playback-state playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read"
+        return util.prompt_for_user_token(
+            config["settings"]["spotify-api"]["USERNAME"],
+            spotify_scope,
+            config["settings"]["spotify-api"]["CLIENT_ID"],
+            config["settings"]["spotify-api"]["CLIENT_SECRET"],
+            spotify_redirect_uri,
+        )
+    except Exception as e:
+        print(f'ERROR: Failed to start spotipy, {e}')
