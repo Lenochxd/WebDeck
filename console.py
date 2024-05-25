68951e65092a98f0be2857cdd7ef49810de4af09
Lenochxd <81lennoch@gmail.com>
2024-05-26 01:37:34 +0200

    Tidy-up: moved `console.py`

--- console.py
+++ console.py
@@ -1,3 +1,9 @@
+"""
+This file is used for debugging purposes and allows sending commands directly to the server
+without the need for a web browser interface. It provides a convenient way to interact with
+the server, test functionality, and troubleshoot issues during development.
+"""
+
 import requests
 import socket
 import json
