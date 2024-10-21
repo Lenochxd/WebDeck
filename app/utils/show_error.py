c19d428d8c5bfdc5f2c4bb5b2f10f0b036602f9e
Lenoch <81lennoch@gmail.com>
2024-10-21 16:34:14 +0200

    Enhance error handling with GitHub issue reporting
    - Added options in the error dialog to open a GitHub issue for reported errors.
    - Integrated functionality to copy error messages to the clipboard for user convenience.
    - Translated prompts for both clipboard copying and GitHub issue reporting.

--- app/utils/show_error.py
+++ app/utils/show_error.py
@@ -1,7 +1,10 @@
+import pyperclip
+import webbrowser
 import ctypes
 import traceback
 import tkinter as tk
 from tkinter import messagebox
+from .languages import text
 
 
 def show_error(message=None, title="WebDeck Error", error=True, exception=None) -> None:
@@ -15,6 +18,30 @@ def show_error(message=None, title="WebDeck Error", error=True, exception=None) 
         root = tk.Tk()
         root.withdraw()
         messagebox.showerror(title, message)
+        
+        if messagebox.askyesno(
+            text("open_github_issue_prompt_title"),
+            text("open_github_issue_prompt_message")
+        ):
+            issue_title = f"[AUTO] {title} - {str(exception)}".replace(" ", "%20")
+            issue_body = (
+                "**Describe the bug**     <!-- (edit this) -->\n"
+                "A clear and concise description of what the bug is.\n\n"
+                "**Steps to reproduce**     <!-- (example) -->\n"
+                "1. Go to '...'\n"
+                "2. Click on '....'\n"
+                "3. Scroll down to '....\n\n\n"
+                "## The error\n"
+                f"{message}"
+            )
+            issue_body = issue_body.replace("\n", "%0A").replace(" ", "%20").replace("#", "%23")
+            webbrowser.open(f"https://github.com/Lenochxd/WebDeck/issues/new?labels=bug&template=bug_report.md&title={issue_title}&body={issue_body}")
+        elif messagebox.askyesno(
+            text("copy_to_clipboard_prompt_title"),
+            text("copy_to_clipboard_prompt_message")
+        ):
+            pyperclip.copy(message)
+        
         root.destroy()
     else:
         ctypes.windll.user32.MessageBoxW(None, message, title, 0)
