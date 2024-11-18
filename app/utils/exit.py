925eaf88df346d19b922e912fe62685b1255c17c
Lenoch <81lennoch@gmail.com>
2024-11-18 16:38:08 +0100

    Fix exit_program from the tray icon in dev mode
    - Changed default value of `force` parameter to `True`.
    - Added logic to terminate Python executables when not running in a frozen state.
    - Improved logging for process termination attempts.

--- app/utils/exit.py
+++ app/utils/exit.py
@@ -6,7 +6,7 @@ import signal
 from .logger import log
 
 
-def exit_program(force=False):
+def exit_program(force=True):
     log.info("Exiting WebDeck...")
     if sys.platform == "win32":
         wmi = win32com.client.GetObject("winmgmts:")
@@ -15,6 +15,11 @@ def exit_program(force=False):
         process_names_to_terminate = ["nircmd.exe"]
         if force:
             process_names_to_terminate.append("webdeck.exe")
+            if not getattr(sys, 'frozen', False):
+                log.debug("Curently not running in a frozen state (not compiled). Adding python executables to termination list.")
+                os.kill(os.getpid(), signal.SIGINT)
+                process_names_to_terminate.append("python.exe")
+                process_names_to_terminate.append("py.exe")
 
         for process in processes:
             process_name = process.Properties_('Name').Value.lower().strip()
@@ -25,9 +30,11 @@ def exit_program(force=False):
                     if result == 0:
                         log.success(f"Process terminated successfully: {process.Properties_('Name').Value}")
                     else:
+                        log.debug(f"Failed to terminate process {process_name} using WMI. Attempting taskkill.")
                         subprocess.Popen(f"taskkill /f /IM {process_name}", shell=True)
                 except Exception as e:
                     log.exception(e, f"Failed to terminate process '{process_name}'")
 
     if not force:
+        log.debug("Force exit not enabled. Sending SIGINT to current process.")
         os.kill(os.getpid(), signal.SIGINT)
