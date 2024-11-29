869690012422a588e1fba59e2ecb477dd458148c
Lenoch <81lennoch@gmail.com>
2024-11-29 14:18:36 +0100

    Fix directory opening logic in opendir function
    Refactored `opendir` function in `opendir.py` to improve directory path normalization and resolution.

--- app/buttons/system/opendir.py
+++ app/buttons/system/opendir.py
@@ -1,35 +1,17 @@
 import os
-import sys
 from subprocess import Popen
 from app.utils.logger import log
 
 
 def opendir(message):
-    # FIXME: works weirdly
-    
+    def normalize_path(path):
+        return path.replace('\\\\', '\\').replace('\\', '/')
+
     path = message.replace("/openfolder", "", 1).replace("/opendir", "", 1).strip()
-    pathtemp = path.replace('\\\\','\\').replace('\\', '/')
-    
-    if not ":" in pathtemp:
-        path = os.path.join(os.getcwd(), path)
-        if not os.path.isdir(path):
-            path = os.path.join(os.path.dirname(sys.executable), path)
-            if not os.path.isdir(path):
-                path = pathtemp
-    else:
-        path = pathtemp
-        
-    path = path.replace('\\\\','\\').replace('\\', '/')
-    
-    # if not path.endswith('/'):
-    #     path += '/'
-        
-    if not os.path.isdir(path):
-        if path.startswith('/'):
-            path = path[1:]
-        path = f"C:/.Code/WebDeck/{path}"
-    
-    path = path.replace('/', '\\')
+    path = normalize_path(path)
+
+    path = os.path.abspath(path)
     log.debug(f"Opening directory: {path}")
     Popen(f'explorer "{path}"')
-    # os.startfile(path)
+
+    return path
