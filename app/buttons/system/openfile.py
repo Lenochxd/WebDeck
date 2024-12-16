fb01876c14c1b928818f1c0b924d4dd9aa739337
Lenoch <81lennoch@gmail.com>
2024-12-16 14:16:09 +0100

    Add cross-platform support for opening files
    - Added platform checks for Windows, Linux, and Mac
    - Implemented open_path function to handle platform-specific file opening

--- app/buttons/system/openfile.py
+++ app/buttons/system/openfile.py
@@ -1,14 +1,23 @@
+from app.utils.platform import is_windows, is_linux, is_mac
 import os
 
 
 def openfile(path):
+    def open_path(path):
+        if is_windows:
+            os.startfile(path)
+        elif is_linux:
+            os.system(f'xdg-open "{path}"')
+        elif is_mac:
+            os.system(f'open "{path}"')
+    
     if "://" not in path and ":" in path:
         initial_path = os.getcwd()
+        file_directory = os.path.dirname(path)
         try:
-            file_directory = os.path.dirname(path)
             os.chdir(file_directory)
-            os.startfile(path)
+            open_path(path)
         finally:
             os.chdir(initial_path)
     else:
-        os.startfile(path)
+        open_path(path)
