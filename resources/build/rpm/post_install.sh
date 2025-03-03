aa9b5fcc60b7d3da96078011b386d3df00e70571
Lenoch <contact.lenoch@gmail.com>
2025-03-03 23:35:56 +0100

    Add desktop icon to RPM build
    - Added creation of desktop icon in `post_install.sh`
    - Added removal of desktop icon in `post_uninstall.sh`

--- resources/build/rpm/post_install.sh
+++ resources/build/rpm/post_install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+# Get the latest version
+latest_version=$(ls /usr/lib | grep webdeck- | sort -V | tail -n 1)
+
+# Create the desktop icon
+cat <<EOF > /usr/share/applications/webdeck.desktop
+[Desktop Entry]
+Name=WebDeck
+Icon=/usr/lib/$latest_version/icon.png
+Exec=/usr/bin/webdeck
+Terminal=false
+Type=Application
+Categories=Utility;
+EOF
+
 update-desktop-database /usr/share/applications
 gtk-update-icon-cache /usr/share/icons/hicolor
--- resources/build/rpm/post_uninstall.sh
+++ resources/build/rpm/post_uninstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+rm -f /usr/share/applications/webdeck.desktop
+
 update-desktop-database /usr/share/applications
 gtk-update-icon-cache /usr/share/icons/hicolor
