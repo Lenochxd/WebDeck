34b847b110244f3c37d1c6034a517c64985a3cf2
Alin Daniel Ferenczi <41358240+AlinDanielFerenczi@users.noreply.github.com>
2026-01-18 18:13:32 +0200

    fix set_speakers_by_name

--- app/buttons/audio/set_system_speaker.py
+++ app/buttons/audio/set_system_speaker.py
@@ -1,27 +1,10 @@
-import pyaudio
-import win32api
-import win32con
+import comtypes
+from pycaw.pycaw import AudioUtilities
+from pycaw.constants import EDataFlow
 
-
-p = pyaudio.PyAudio()
 def set_speakers_by_name(speakers_name):
-    # TODO: (not working rn)
-    device_count = p.get_device_count()
-
-    for i in range(device_count):
-        device_info = p.get_device_info_by_index(i)
-        if device_info["name"].lower().find(speakers_name.lower()) != -1:
-            # Select the found audio device
-            win32api.SendMessage(
-                win32con.HWND_BROADCAST,
-                win32con.WM_APPCOMMAND,
-                0,
-                win32api.LPARAM(0x30292),
-            )
-            win32api.SendMessage(
-                win32con.HWND_BROADCAST,
-                win32con.WM_APPCOMMAND,
-                0,
-                win32api.LPARAM(0x30290 + i),
-            )
-            break
+    comtypes.CoInitialize()
+    devices = AudioUtilities.GetAllDevices(data_flow=EDataFlow.eRender.value)
+    for device in devices:
+        if device.FriendlyName == speakers_name:
+            AudioUtilities.SetDefaultDevice(device.id)
